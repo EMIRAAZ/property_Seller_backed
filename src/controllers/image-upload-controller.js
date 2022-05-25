9e49a8974c46d22f9124349eec766ae6e608ac3d
vishnudeviravi <ivishnudr@gmail.com>
2022-05-25 14:01:10 +0530

    ADDED IMAGE URL

--- src/controllers/image-upload-controller.js
+++ src/controllers/image-upload-controller.js
@@ -1,7 +1,11 @@
 async function uploadImage(req, res) {
   const { file } = req;
 
-  const downloadURL = `${'127.0.0.1:3000'}/${file.path}`;
+  let downloadURL;
+
+  if (process.env.NODE_ENV === 'development') {
+    downloadURL = `${'127.0.0.1:3000'}/${file.path}`;
+  } else downloadURL = `${'http://3.94.127.24:3000'}/${file.path}`;
 
   return res.status(201).json({
     status: 201,
