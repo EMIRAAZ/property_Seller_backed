d08b71f416105cd318fb60feb075f7b3b6948bc9
vishnudeviravi <ivishnudr@gmail.com>
2022-06-23 10:24:00 +0530

    ADDING OFFPLAN MODEL

---
+++ src/models/OffPlan.js
@@ -0,0 +1,67 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../database/dbConnection');
+
+const Agent = require('./Agent');
+const Agency = require('./Agency');
+const Address = require('./Address');
+
+const OffPlan = sequelize.define(
+  'offplan',
+  {
+    id: {
+      type: Sequelize.STRING(12),
+      allowNull: false,
+      primaryKey: true,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.STRING(2000),
+      allowNull: false,
+    },
+    images: {
+      type: Sequelize.ARRAY(Sequelize.STRING(2000)),
+      allowNull: false,
+    },
+    amenities: {
+      type: Sequelize.ARRAY(Sequelize.STRING(1000)),
+      allowNull: true,
+    },
+    availability: {
+      //[{noOfBedroom:4,type:"apartment",floorPlan:""}]
+      type: Sequelize.ARRAY(Sequelize.JSON),
+      allowNull: true,
+    },
+    paymentPlan: {
+      type: Sequelize.ARRAY(Sequelize.STRING(200)),
+      allowNull: true,
+    },
+    brochurePDF: {
+      type: Sequelize.STRING(2000),
+      allowNull: true,
+    },
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE,
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+OffPlan.belongsTo(Agent, {
+  foreignKey: 'agentId',
+  as: 'agent',
+});
+
+OffPlan.belongsTo(Address, {
+  foreignKey: 'addressId',
+  as: 'address',
+});
+OffPlan.belongsTo(Agency, {
+  foreignKey: 'agencyId',
+  as: 'agency',
+});
+
+module.exports = OffPlan;
