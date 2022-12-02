81d52815ee4bea7b5324a97ea9842cb390312820
vishnudeviravi <ivishnudr@gmail.com>
2022-12-02 18:13:38 +0530

    ADDING CHANGES

--- src/controllers/image-upload-controller.js
+++ src/controllers/image-upload-controller.js
@@ -7,7 +7,7 @@ async function uploadImage(req, res) {
 
   if (process.env.NODE_ENV === 'development') {
     downloadURL = `${'127.0.0.1:3000'}/${file.path}`;
-  } else downloadURL = `${'https://api.uaeassistant.com'}/${file.path}`;
+  } else downloadURL = `${'http://54.163.214.5'}/${file.path}`;
 
   return res.status(201).json({
     status: 201,
