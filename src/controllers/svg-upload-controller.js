15b7eb1ca53d40020d8d5eb13bd11a8f3f691717
EMIRAAZ <80477631+EMIRAAZ@users.noreply.github.com>
2023-12-08 06:52:07 +0530

    Update svg-upload-controller.js
    Change ip address to domain name with https

--- src/controllers/svg-upload-controller.js
+++ src/controllers/svg-upload-controller.js
@@ -5,7 +5,7 @@ async function uploadSVG(req, res) {
 
   if (process.env.NODE_ENV === 'development') {
     downloadURL = `${'127.0.0.1:3000'}/${file.path}`;
-  } else downloadURL = `${'http://3.94.127.24:3000'}/${file.path}`;
+  } else downloadURL = `${'https://www.propertyseller.ae'}/${file.path}`;
 
   return res.status(201).json({
     status: 201,
